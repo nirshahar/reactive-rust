16c50c83911494ec2fe22a74963d36bce2b78ee0
nir <nirshahar@mail.tau.ac.il>
2023-04-07 19:31:00 +0300

    Add basic structure for multi-threaded events

--- src/observable.rs
+++ src/observable.rs
@@ -1,11 +1,15 @@
+use crossbeam_queue::SegQueue;
+
 pub struct Observable<'a, Item> {
     observers: Vec<Box<dyn Fn(&Item) + 'a>>,
+    event_queue: SegQueue<&'a Item>,
 }
 
 impl<'a, Item> Observable<'a, Item> {
     pub fn new() -> Self {
         return Self {
             observers: Vec::new(),
+            event_queue: SegQueue::new(),
         };
     }
 
@@ -13,7 +17,21 @@ impl<'a, Item> Observable<'a, Item> {
         self.observers.push(Box::new(observer));
     }
 
-    pub fn emit(&self, item: &Item) {
+    pub fn publish(&self, item: &'a Item) {
+        self.event_queue.push(item);
+    }
+
+    pub fn emit_once(&self) -> bool {
+        if let Some(item) = self.event_queue.pop() {
+            self.emit_item(item);
+
+            true
+        } else {
+            false
+        }
+    }
+
+    pub fn emit_item(&self, item: &'a Item) {
         for obs in self.observers.iter() {
             obs(item);
         }
