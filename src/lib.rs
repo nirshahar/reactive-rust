f3f79c097d5ef9d11a3973dfc39650a61a0dd3f4
nir <nirshahar@mail.tau.ac.il>
2023-04-12 00:56:48 +0300

    Add a small sanity test

--- src/lib.rs
+++ src/lib.rs
@@ -4,5 +4,20 @@ pub mod scheduler;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::observable::Observable;
+
+    #[test]
+    fn test() {
+        let mut total = 0;
+
+        {
+            let mut observable = Observable::new();
+
+            observable.publish(5);
+            observable.observe(|event| total += 3 * event);
+            observable.publish(2);
+        }
+
+        assert_eq!(total, 6);
+    }
 }
