48ef0278e1077b227db7a11a4d15ad77eeaad5cf
nir <nirshahar@mail.tau.ac.il>
2023-04-21 19:00:04 +0300

    Added another test

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,10 @@ pub mod scheduler;
 
 #[cfg(test)]
 mod tests {
-    use crate::observable::Observable;
+    use crate::{
+        observable::Observable,
+        scheduler::{MultiThreadedScheduler, Scheduler},
+    };
 
     #[test]
     fn test() {
@@ -15,9 +18,23 @@ mod tests {
 
             observable.publish(5);
             observable.observe(|event| total += 3 * event);
+            observable.publish(1);
+            observable.observe(|event| println!("Got an event: {event}!"));
             observable.publish(2);
         }
 
-        assert_eq!(total, 6);
+        assert_eq!(total, 9);
+    }
+
+    #[test]
+    fn test_scheduler() {
+        let mut observable: Observable<&str, MultiThreadedScheduler<_>> =
+            Observable::with_scheduler();
+
+        observable.observe(|event| println!("woosh: {}", event));
+
+        observable.publish("sus");
+        observable.publish("eee");
+        observable.publish("cool!");
     }
 }
